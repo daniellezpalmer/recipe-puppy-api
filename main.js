48412adbd622c522ea2e788424918f564e42b0d8
Danielle Palmer <daniellezpalmer@gmail.com>
2017-10-14 17:14:38 -0500

    Added fetch to puppy recipes api, including query selector for lower half and creating tmplate literal div for results

--- main.js
+++ main.js
@@ -4,3 +4,33 @@ submit.addEventListener('click', function() {
   let input = document.querySelector('#input');
   console.log(input);
 let searchInput = input.value;
+
+fetch(
+  "http://recipepuppyproxy.herokuapp.com/api/?q=" + searchInput)
+  .then(function(response) {
+    if (response.status !== 200) {
+      console.log("Broken: " + response.status);
+      return;
+    }
+    response.json().then(function(data) {
+      let html = "";
+      let recipes = data.results;
+      console.log(recipes);
+      let lowerHalf = document.querySelector('#lowerHalf')
+      for (let i = 0; i < recipes.length; i++) {
+        let href = recipes[i].href;
+        let title = recipes[i].title;
+        let thumbnail = recipes[i].thumbnail;
+        if (thumbnail === ""){
+          thumbnail = 'http://via.placeholder.com/350x150'
+        }
+      html += `<div class="container">
+              <a href="${href}">Recipe</a>
+              <p>${title}</p>
+              <img src="${thumbnail}">
+              </div>`
+      lowerHalf.innerHTML = html;
+      }
+    })
+  })
+})
