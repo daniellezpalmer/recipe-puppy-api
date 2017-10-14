80fcda7e86d6ea52b22910411d8b0e3630cde180
Danielle Palmer <daniellezpalmer@gmail.com>
2017-10-14 17:13:04 -0500

    Added click event for search input

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 let submit =document.querySelector('#button');
+
+submit.addEventListener('click', function() {
+  let input = document.querySelector('#input');
+  console.log(input);
+let searchInput = input.value;
